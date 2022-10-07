57480065a0ef7c51cba0f041ea0a79093f456322
gabrielvalencia <gabev13@gmail.com>
2022-10-07 06:57:56 -0500

    barebones

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,6 +1,10 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc {
+    @Override
+    public void spinDisc() {
+        System.out.println("A DVD spins at a rate of 570 - 1600 rpm.");
+    }
 
     // TODO: Implement your custom interface.
 
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args){
 
         // TODO: Declare and initialize a CD and a DVD object.
+        CD hs = new CD();
+        DVD hsm = new DVD();
+
+        hs.spinDisc();
+        hsm.spinDisc();
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
